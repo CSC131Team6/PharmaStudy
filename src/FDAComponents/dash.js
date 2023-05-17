cc215b9229c81d2188d7477bb19d8abfea099d9d
CSC131Team6 <125523843+CSC131Team6@users.noreply.github.com>
2023-05-17 01:34:16 -0700

    Update dash.js
    added boxes where people can add things and made the welcome font larger

--- src/FDAComponents/dash.js
+++ src/FDAComponents/dash.js
@@ -4,28 +4,29 @@ import { useUserContext } from "../config/FDAUserContext";
 import useJaneHopkins from "../hooks/useJaneHopkins";
 import useFDA from "../hooks/useFDA";
 import useBavaria from "../hooks/useBavaria";
-import "./fda.modules.css"
+import "./fda.modules.css";
 
 function Dashboard() {
-  const { user, logoutUser } = useUserContext();
-  return (
-    <>
-      <div>
-        <h2>{user.email}</h2>
-        <button onClick={logoutUser}>Log out</button>
-      </div>
-    <div
-      style={{
-      position: "absolute",
-      left: "50%",
-      top: "60%",
-      transform: "translate(-50%, -50%)",
-    }}
-    >
-    <h1>Welcome to the FDA Home Page</h1>
-    </div>
-    </>
-  );
+    const { user, logoutUser } = useUserContext();
+
+    return (
+        <>
+            <div>
+                <h2>{user.email}</h2>
+                <button onClick={logoutUser}>Log out</button>
+            </div>
+
+            <div className="fdaform">
+                <h1 className="fdaform__title">Welcome to the FDA Home Page</h1>
+                <div className="fdaform__container">
+                    <h2 className="fdaform__subtitle">Form Title</h2>
+                    <form className="fdaform__form">
+                        {/* form elements here */}
+                    </form>
+                </div>
+            </div>
+        </>
+    );
 };
 
 export default Dashboard;
