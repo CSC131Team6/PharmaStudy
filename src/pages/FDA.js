6f99ec26eebd993416ac82d6dafd6526059a5093
CSC131Team6 <125523843+CSC131Team6@users.noreply.github.com>
2023-05-17 02:43:58 -0700

    Update FDA.js
    reverted to normal and placed code into correct place

--- src/pages/FDA.js
+++ src/pages/FDA.js
@@ -1,44 +1,16 @@
-import React from "react";
+import React from "react"
 import Auth from "../FDAComponents/auth";
 import Dashboard from "../FDAComponents/dash";
 import { useUserContext } from "../config/FDAUserContext";
 
 export default function FDA() {
+
     const { user, loading, error } = useUserContext();
 
     return (
         <div className="App">
-            <nav className="nav">
-                <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/7/7d/Food_and_Drug_Administration_logo.svg/1280px-Food_and_Drug_Administration_logo.svg.png" alt="FDA logo" style={{ width: '60px', height: '25px' }} />
-                <ul>
-                    <li>
-                        <a href="#">Retrieve Smart Contract Parameters</a>
-                    </li>
-                    <li>
-                        <a href="#">Patient Profile</a>
-                    </li>
-                    <li>
-                        <a href="#">Notify Study Completion</a>
-                    </li>
-                    <li>
-                        <a href="#">Generate Post-Trial Report</a>
-                    </li>
-                </ul>
-            </nav>
-            <div className="container">
-                {error && <p className="error">{error}</p>}
-                {loading ? (
-                    <h2>Loading...</h2>
-                ) : (
-                    <>
-                        {user ? (
-                            <Dashboard />
-                        ) : (
-                            <Auth />
-                        )}
-                    </>
-                )}
-            </div>
+            {error && <p className="error">{error}</p>}
+            {loading ? <h2>Loading...</h2> : <> {user ? <Dashboard /> : <Auth />} </>}
         </div>
     );
-}
+};
