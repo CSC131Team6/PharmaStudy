5785dbc62688a1ca6b04b39ea582158257ad8a8c
CSC131Team6 <125523843+CSC131Team6@users.noreply.github.com>
2023-05-18 09:21:47 -0700

    Update patientTable.js
    Currently trying to add more Patient data off of Vendia

--- src/JaneHopkinsComponents/JaneHopkins/patientTable.js
+++ src/JaneHopkinsComponents/JaneHopkins/patientTable.js
@@ -7,6 +7,9 @@ import TableContainer from '@mui/material/TableContainer';
 import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
 import Paper from '@mui/material/Paper';
+import useJaneHopkins from "../../hooks/useJaneHopkins";
+
+
 
 const StyledTableCell = styled(TableCell)(({ theme }) => ({
   [`&.${tableCellClasses.head}`]: {
@@ -33,12 +36,69 @@ function createData(name, calories, fat, carbs, protein) {
 }
 
 const rows = [
+  createData('Hello')
+]
+
+function RealDataPull () {
+  const { entities } = useJaneHopkins();
+  console.log(entities);
+
+  //USING REAL LIST
+  const realList = async () => {
+    //console.log('testing1');
+    const gettingList = await entities.patient.list();
+    //console.log('testing2');
+    gettingList.map((persons) => <li>{persons}</li>)
+    console.log(gettingList);
+  }
+
+  realList();
+  
+  return (
+    <div>
+      <ul>{realList}</ul>
+    </div>
+  );
+}
+
+/*const rows = [
   createData('Jason Cha', 1996, 6532483279.0, 400, 90.0),
   createData('Jordyn Rista', 1237, 6238193821.0, 430, 120.3),
   createData('Ian Moose', 2003, 163428865.0, 550, 143.0),
   createData('Danny Devito', 4003, 36547363.7, 460, 230.3),
   createData('He Man', 1010, 166436373.0, 395, 476.9),
-];
+];*/
+
+/*export default function PatientTable() {
+  return (
+    <TableContainer component={Paper}>
+      <Table sx={{ minWidth: 700 }} aria-label="customized table">
+        <TableHead>
+          <TableRow>
+            <StyledTableCell>Patient</StyledTableCell>
+            <StyledTableCell align="right">DOB</StyledTableCell>
+            <StyledTableCell align="right">Insurance Number</StyledTableCell>
+            <StyledTableCell align="right">Height</StyledTableCell>
+            <StyledTableCell align="right">Weight</StyledTableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {rows.map((row) => (
+            <StyledTableRow key={row.name}>
+              <StyledTableCell component="th" scope="row">
+                {row.name}
+              </StyledTableCell>
+              <StyledTableCell align="right">{row.calories}</StyledTableCell>
+              <StyledTableCell align="right">{row.fat}</StyledTableCell>
+              <StyledTableCell align="right">{row.carbs}</StyledTableCell>
+              <StyledTableCell align="right">{row.protein}</StyledTableCell>
+            </StyledTableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </TableContainer>
+  );
+}*/
 
 export default function PatientTable() {
   return (
